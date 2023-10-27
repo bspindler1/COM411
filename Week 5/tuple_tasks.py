0f7c7379d73005c2f6891959b4e4d4d76f1c82fd
Ben Spindler <2spinb05@solent.ac.uk>
2023-10-27 14:57:27 +0100

    Added code to demonstrate how to return a tuple from a function.

--- Week 5/tuple_tasks.py
+++ Week 5/tuple_tasks.py
@@ -8,5 +8,17 @@ def run_task1():
     print(f"Minimum likelihood of falling: {value}%")
 
 
+def likelihood_min_max():
+    likelihoods = (50, 38, 27, 99, 4)
+    return min(likelihoods), max(likelihoods)
+
+
+def run_task2():
+    data = likelihood_min_max()
+    print(f"Minimum likelihood of falling: {data[0]}%")
+    print(f"Maximum likelihood of falling: {data[1]}%")
+
+
+run_task2()
 
 
