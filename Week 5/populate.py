dc68c78268f70bef1e34c2955e95ec7441781942
Ben Spindler <2spinb05@solent.ac.uk>
2023-10-27 14:41:00 +0100

    Added code to demonstrate how to populate a list.

---
+++ Week 5/populate.py
@@ -0,0 +1,23 @@
+def directions():
+    steps = ["Move Forward", "Move Backward", "Turn Left", "Turn Right"]
+    return steps
+
+
+def menu_and_input():
+    print("Please select a direction:")
+    direction = directions()
+    for index in range(len(direction)):
+        print(f"{index}: {direction[index]}")
+    response = int(input())
+    return direction[response]
+
+
+def run_task4():
+    route = []
+    print("Working out escape route...")
+    for index in range(5):
+        route.append(menu_and_input())
+    print(f"Escape route: {route}")
+
+
+run_task4()
