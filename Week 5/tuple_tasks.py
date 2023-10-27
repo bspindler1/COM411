8f8c3841330e9f04c7b95a4bb857dae36c7df9bc
Ben Spindler <2spinb05@solent.ac.uk>
2023-10-27 14:50:24 +0100

    Added code to demonstrate a simple tuple.

---
+++ Week 5/tuple_tasks.py
@@ -0,0 +1,12 @@
+def likelihood():
+    likelihoods = (50, 38, 27, 99, 4)
+    return min(likelihoods)
+
+
+def run_task1():
+    value = likelihood()
+    print(f"Minimum likelihood of falling: {value}%")
+
+
+
+
