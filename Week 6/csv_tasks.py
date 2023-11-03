f08898a38d3ec90546806aae4d264bc7802cea40
bspindler1 <spindlerben2005@gmail.com>
2023-11-03 14:45:04 +0000

    Added code to extract data from the CSV file.

--- Week 6/csv_tasks.py
+++ Week 6/csv_tasks.py
@@ -13,6 +13,24 @@ def read_csv(path):
 
 
 def run_task1():
-    read_csv("clothing.csv")
+    read_csv("C:\Users\mroak\vscode\COM411\Week 6\clothing.csv")
 
-run_task1()
+
+def extract(path):
+    print("Extracting...")
+    with open(path) as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)
+        names = ""
+        for values in csv_reader:
+            names += f"{values[1]}\n{names}"
+    print("Done!")
+    print(f"The extracted items are :\n{names}")
+
+
+def run_task2():
+    extract("C:\Users\mroak\vscode\COM411\Week 6\clothing.csv")
+
+
+run_task2()
+
