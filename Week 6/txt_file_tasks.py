2f69a925d619e024fe6d5fd51db06e66868c50d2
bspindler1 <spindlerben2005@gmail.com>
2023-11-03 13:48:11 +0000

    .

--- Week 6/txt_file_tasks.py
+++ Week 6/txt_file_tasks.py
@@ -2,16 +2,40 @@ import os
 
 
 def cwd():
-    path = os.getcwd()
-    print(f"The current working directory is {path}")
-    print(f"The directory contains the following:")
-    for file in os.listdir(path):
-        print(file)
+  path = os.getcwd()
+  print(f"The current working directory is {path}")
+  print(f"The directory contains the following:")
+  for file in os.listdir(path):
+    print(file)
 
 
 def run():
-    print("Processing...")
-    cwd()
+  print("Processing...")
+  cwd()
 
 
-run()
+def display_chars(file_path, num):
+    with open(file_path) as file:
+        data = file.read(num)
+        print(data)
+
+
+def display_line(file_path):
+    with open(file_path) as file:
+        data = file.readline().strip()
+        print(data)
+
+
+def display_text(file_path):
+    with open(file_path) as file:
+        data = file.read()
+        print(data)
+
+
+def run_task2():
+    display_chars("library.txt", 5)
+    display_line("library.txt")
+    display_text("library.txt")
+
+
+run_task2()
