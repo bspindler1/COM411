b6b85719848cd1465abe054549ca4ade0f940bca
Ben Spindler <2spinb05@solent.ac.uk>
2023-11-10 14:17:24 +0000

    Added code to demonstrate a simple set.

---
+++ Week 7/set_tasks.py
@@ -0,0 +1,9 @@
+def observed():
+    observations = {"Car", "Sky Scraper", "Sky Scraper", "Bike", "House", "House"}
+    return observations
+
+def run_task1():
+    print(observed())
+
+run_task1()
+
