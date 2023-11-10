ae24acef173edf5c656e81bf4929c522e1d19380
Ben Spindler <2spinb05@solent.ac.uk>
2023-11-10 14:56:30 +0000

    Added code to demonstrate how to manipulate and sort a set.

--- Week 7/set_tasks.py
+++ Week 7/set_tasks.py
@@ -9,7 +9,7 @@ def run_task1():
 def observed_items():
   observations = []
 
-  for count in range(7):
+  for count in range(5):
     print("Please enter an observation:")
     observations.append(input())
 
@@ -29,7 +29,32 @@ def run_task2():
   for data in observations_set:
     print(f"{data[0]} observed {data[1]} times.")
 
-run_task2()
+
+def remove_observations(observations):
+    while True:
+        print("Would you like to remove observations?")
+        response = input()
+        if response == "yes":
+            print("Enter the name of the observation")
+            remove = input()
+            observations.remove(remove)
+        else:
+            break
+
+def run_task3():
+    observations = observed_items()
+    remove_observations(observations)
+
+    observations_set = set()
+    for observation in observations:
+        data = (observation, observations.count(observation))
+        observations_set.add(data)
 
 
+
+    for data in sorted(observations_set):
+        print(f"{data[0]} observed {data[1]} times.")
+
+
+run_task3()
 
