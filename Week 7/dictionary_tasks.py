7ade4faeb3138e211c5f07a2222b3445132200e4
Ben Spindler <2spinb05@solent.ac.uk>
2023-11-10 15:19:35 +0000

    Added code to demonstrate how to create a dictionary of dictionaries.

--- Week 7/dictionary_tasks.py
+++ Week 7/dictionary_tasks.py
@@ -32,4 +32,29 @@ def display_items(data):
     for key, value in data.items():
         print(f"{key}: {value}")
 
-run()
+
+def short_pattern():
+    pattern = {"sequence": "101", "occurrences": 5}
+    return pattern
+
+
+def medium_pattern():
+    pattern = {"sequence":"111000", "occurrences": 25}
+    return pattern
+
+
+def long_pattern():
+    pattern = {"sequence":"1100110011001100", "occurrences":50}
+    return pattern
+
+
+def run_task3():
+    print("Analysing patterns...")
+    patterns = {"short sequence":short_pattern(), "medium sequence":medium_pattern(), "long sequence":long_pattern()}
+
+    for keys, value in patterns.items():
+        print(f"{keys}: {value}")
+
+
+run_task3()
+
